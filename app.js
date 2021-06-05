4d59dc1a4c6d742f87abac15f902cc43f2309570
Shane Chou <shaioking@gmail.com>
2021-06-06 02:11:00 +0800

    Set 2

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
+const User = require("./models/user");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const flash = require("connect-flash");
+const { nextTick } = require("process");
+// const { rawListeners } = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+//copy down
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+// authenticate user
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//copy up
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.err_msg = req.flash("err_msg");
+  next();
+});
 
 // connect to mongoDB
 mongoose
@@ -32,6 +61,34 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res, next) => {
+  let { fullname, usertype, username, password, password2 } = req.body;
+  //password matching
+  if (password !== password2) {
+    req.flash("err_msg", "Passwords don't match. Please check.");
+    res.redirect("/register");
+  } else {
+    //email not registered yet
+    try {
+      let foundUser = await User.findOne({ username });
+      if (foundUser) {
+        req.flash("err_msg", "Email has been registered. Please check.");
+        res.redirect("/register");
+      } else {
+        let newUser = new User(req.body);
+        await User.register(newUser, password);
+        req.flash(
+          "success_msg",
+          "Account has been created. You can login now."
+        );
+        res.redirect("/login");
+      }
+    } catch (err) {
+      next();
+    }
+  }
 });
 
 app.listen(3000, () => {
